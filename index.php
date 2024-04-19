8a3abc99e5d3f94c429f87e833a095b23cbd0d3d
Poojakumawet <115582158+Poojakumawet@users.noreply.github.com>
2024-04-19 08:47:18 -0700

    Add files via upload
    Create Home page

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    
+</title>
+<link rel="stylesheet" type="text/css" href="bank.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    </head>  
+</head>
+<body>
+    <div class="new">
+        <img src="logo.jpg" alt="Logo" class="logo">
+        <nav>
+         <ul>
+                <li><a href="index.html" class="icon-link"><i class="fa fa-home"></i> Home</a></li>
+                <li>  <a href="contact.html" class="icon-link"><i class="fa fa-envelope"></i> Contact</a></li>
+                <li><a href="">Fixed Deposite</a></li>
+                <li><a href="account.html" class="account-link">
+                    <i class="fa fa-user"></i> My Account</a></li>
+            </ul>
+         </nav>
+    </div>
+    <div id="new">
+    <div class="text-overlay">
+            <h2> Welcome to our Banking system </h2>
+            <button id="button1">View All customer</button><br><br>
+    
+            <button id="button2">Transfer Money </button><br><br>
+       
+            <a href="view_transfers.php">Transfer History </a>  
+          </div>   
+          <script src="script.js"></script>     
+</div>
+        <footer>
+        <div class="footer-content">
+            <p>&copy; <?php echo date('Y'); ?> Spark foundation intership Company. All rights reserved.</p>
+            <p>Contact us at: <a href="mailto:sparkfoundation@mycompany.com">sparkfoundation@mycompany.com</a></p>
+            <p>
+                Follow us on:
+                <a href="https://www.facebook.com/mycompany">Facebook</a> |
+                <a href="https://twitter.com/mycompany">Twitter</a>
+            </p>
+        </div>
+       
+        
+</body>
+</html>
+
+
+
+
+
+
